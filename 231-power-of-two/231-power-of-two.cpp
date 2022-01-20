bb2ff425b875aec4fd889a7f728b915a5e0d3e84
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-01-20 21:13:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.25%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n>1 and ceil(log2(n)) == floor(log2(n)) )
-            return true;
-        
-        if(n==1)    return true;
-        return false;
-    }
+    if(n <= 0) return false;
+    return !(n&(n-1));
+}
 };
