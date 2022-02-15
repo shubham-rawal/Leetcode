9ffb2f6e711d17acd36e3feee29777d22364c481
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-02-15 21:39:02 +0530

    Time: 27 ms (53.51%), Space: 17 MB (50.83%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i+=2)
-        {
-            if(nums[i]!=nums[i-1])
-                return nums[i-1];
-        }
-        return nums[nums.size()-1];
+       //using bit manipulation
+        /*
+            a^a^a^a.... even times = 0 and a^a^a...odd times = a;
+        */
+        int ans = 0;
+        
+        for(auto x: nums)   ans^=x;
+        return ans;
     }
 };
