707924b00cb6172d2a0a6ba458bf2d9936d20ce2
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-02-22 18:52:21 +0530

    Time: 4 ms (47.34%), Space: 5.9 MB (92.06%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -5,6 +5,8 @@ public:
         int ans = 0;
         for(int i = 0; i<n; i++){
             int idx = columnTitle[i] -'A' + 1;
+            
+            //this is similar to breaking down 999 as 9*10*10 + 9*10 + 9 except that in this case our base is 26
             ans = 26*ans + idx;
         }
         return ans;
