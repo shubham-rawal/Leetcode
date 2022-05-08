eb5c3c68cbf3fc2b7860ab9bd8b4c3678ab64fe2
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-05-08 16:08:00 +0530

    Time: 113 ms (84.59%), Space: 67.8 MB (12.11%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,24 +4,23 @@ public:
         int cs = 0, ms = INT_MIN, flag = 0;
         
         //if all elements in the subarray are -ve, return the least negative element since contiguous subarray is required.
-    for(int i = 0; i < nums.size(); i++){
-        if(nums[i]>=0){
-            flag = 1;
-            ms = 0;
-            break;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]>=0){
+                flag = 1;
+                ms = 0;
+                break;
+            }
+            ms = max(ms, nums[i]);
         }
-        ms = max(ms, nums[i]);
-    }
-      if(flag == 1){
-        for(int i = 0 ; i< nums.size(); i++){
-            cs += nums[i];
+        
+        if(flag == 1){
+            for(int i = 0; i<nums.size(); i++){
+                cs+=nums[i];
+                if(cs<0)    cs = 0;
             
-            if(cs<0)
-                cs = 0;
-            
-            ms = max(ms, cs);
+                ms = max(ms, cs);
+            }
         }
-      }
         return ms;
     }
 };
