485d8eaa94b3e89588fbf758f2c829b2fca539ea
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-05-18 13:11:53 +0530

    Time: 37 ms (87.99%), Space: 19.1 MB (46.27%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -13,6 +13,10 @@ public:
         v.second = intervals[0][1];
         
         for(int i = 0; i<intervals.size()-1; i++){
+            
+                /*
+                    we have to compare the second element of the pair with the first of the next interval element. To check if the interval overlaps the already saved pair
+                */
             if(intervals[i+1][0] <= v.second){
                 // v.first = min(v.first, intervals[i+1][0]);
                 v.second = max(v.second, intervals[i+1][1]);
