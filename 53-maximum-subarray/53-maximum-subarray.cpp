230fc2f20d64bfe1a4bda07c4e414d076eec657a
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-06-25 01:10:20 +0530

    Time: 176 ms (35.85%), Space: 67.8 MB (10.96%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int cs = 0, ms = INT_MIN, flag = 0;
+        //kadane's algorithm
+        /*
+            approach :
+            we maintain a currentSum and a maximumSum.
+        */
         
-        //if all elements in the subarray are -ve, return the least negative element since contiguous subarray is required.
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i]>=0){
-                flag = 1;
-                ms = 0;
-                break;
-            }
-            ms = max(ms, nums[i]);
+        int cs = 0, ms = INT_MIN;
+        //to rule out the possibility of all elements being negative,
+        ms = *max_element(nums.begin(), nums.end());
+        if(ms<0)    return ms;
+        //now maintaining cs
+        for(auto x: nums){
+            cs+=x;
+            if(cs<0)    cs = 0;
+            ms = max(ms, cs);
         }
         
-        if(flag == 1){
-            for(int i = 0; i<nums.size(); i++){
-                cs+=nums[i];
-                if(cs<0)    cs = 0;
-            
-                ms = max(ms, cs);
-            }
-        }
-        return ms;
+        return ms;  
     }
 };
