461c49e6205c2a552fe61dab2d536a57f648b511
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-06-25 00:49:53 +0530

    Time: 38 ms (6.59%), Space: 13.1 MB (81.74%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    
     void setZeroes(vector<vector<int>>& matrix) {
-        //number of rows and columns
+        //m = number of rows
         int m = matrix.size();
+        //n = number of cols
         int n = matrix[0].size();
         
-        //we take 2 dummy arrays to keep track whether the particular row or column has a 0 or not
-        vector<int> dummy1(m, -1);
-        vector<int> dummy2(n, -1);
+        //approach : create 2 vectors, one to maintain row and another to maintain column
+        vector<int> row(m, -1);
+        vector<int> col(n, -1);
         
+        //iterate through the matrix and see where the 0's are
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    dummy1[i] = 0;
-                    dummy2[j] = 0;
+                    row[i] = 0;
+                    col[j] = 0;
                 }
             }
         }
         
-        //now all the indices that have 0 in dummy1 refer to the rows that need to be made 0 in the matrix.
-        //similarly all the indices that have 0 in dummy2 refer to the columns that need to be made 0
-        //i.e. if dummy[i] == 0 || dummy[j] == 0, matrix[i][j] = 0
-        
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(dummy1[i] == 0 or dummy2[j] == 0){
+                if(row[i] == 0 or col[j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        
-        return;
     }
 };
