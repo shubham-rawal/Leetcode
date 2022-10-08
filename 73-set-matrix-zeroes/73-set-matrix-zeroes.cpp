205c92f2216c6a9a96ee650b32ce12160cabc933
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-10-08 15:46:52 +0530

    Time: 27 ms (50.33%), Space: 13.3 MB (20.14%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //m = number of rows
+        /*
+            approach:
+            we will create 2 vectors, row and column.
+            Row vector will store all the row indexes where 0 is present
+            Column vector will store all the column indexes where 0 is present.
+            Now, on changing the matrix, if the row[i] == true(i.e. that row contains the 0) OR column[j] == 0(i.e. that column contains the 0), we mark the entire row and column as 0.
+        */
+        
+        //number of rows
         int m = matrix.size();
-        //n = number of cols
+        //number of cols
         int n = matrix[0].size();
+            
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
         
-        //approach : create 2 vectors, one to maintain row and another to maintain column
-        vector<int> row(m, -1);
-        vector<int> col(n, -1);
-        
-        //iterate through the matrix and see where the 0's are
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
+        //1st iteration
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
+            
         
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(row[i] == 0 or col[j] == 0){
+        //2nd iteration
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(row[i] or col[j]) {
                     matrix[i][j] = 0;
                 }
             }
         }
+
     }
 };
