e84d3540aa1991a0cc2ad6cdfdfaf936393df45d
Shubham Rawal <shubhamrawal2001@gmail.com>
2022-10-08 15:58:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (90.38%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -17,18 +17,19 @@ public:
         //creating the ans matrix
         vector<vector<int>> ans;
         
-        //first row pushed back to the matrix
+        //first row is always 1
         ans.push_back({1});
         
         //for every subsequent row
-        for(int i = 1; i<numRows; i++){
+        for(int i=1; i<numRows; i++) {
+            //creating a new row to be pushed
             vector<int> row;
-            for(int j = 0; j<=i; j++){
-                if(j == 0 or j == i){
+            
+            for(int j = 0; j<=i; j++) {
+                if(j==0 or j==i) {
                     row.push_back(1);
-                }
-                else{
-                    row.push_back(ans[i-1][j-1] + ans[i-1][j]);
+                } else {
+                    row.push_back(ans[i-1][j] + ans[i-1][j-1]);
                 }
             }
             ans.push_back(row);
